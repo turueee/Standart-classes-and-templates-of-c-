ba63da8e9d2a700f6ad188fc0c2b2f4643ea8cd4
turueee <turushkine@inbox.ru>
2025-08-03 22:59:27 +0300

    Add TAllocator

---
+++ TAllocator/TAllocator.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class TAllocator
+{
+public:
+	using value_type = T;
+	using pointer = T*;
+	using const_pointer = const T*;
+	using reference = T&;
+	using const_reference = const T&;
+	using size_type = size_t;
+	using difference_type = ptrdiff_t;
+
+	template <typename U>
+	struct rebind
+	{
+		using other = TAllocator<U>;
+	};
+
+	constexpr TAllocator() noexcept = default;
+	constexpr TAllocator(const TAllocator&) noexcept = default;
+	template<typename U>
+	constexpr TAllocator(const TAllocator<U>&) noexcept;
+	~TAllocator() = default;
+
+	[[nodiscard]] constexpr T* Allocate(size_type n);
+	constexpr void Deallocate(pointer p, size_type n);
+
+	constexpr bool operator== (const TAllocator&) const noexcept;
+};
+
+template<typename T>
+inline constexpr T* TAllocator<T>::Allocate(size_type n)
+{
+	return static_cast<pointer>(::operator new(n*sizeof(value_type)));
+}
+
+template<typename T>
+inline constexpr void TAllocator<T>::Deallocate(pointer p, size_type n)
+{
+	::operator delete(p,n*sizeof(T));
+}
+
+template<typename T>
+inline constexpr bool TAllocator<T>::operator==(const TAllocator&) const noexcept
+{
+	return true;
+}
